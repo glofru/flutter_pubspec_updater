e5525ef20a56f0eed8281ca3018698065ada1bb7
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2021-10-08 12:03:27 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="Flutter pubspec updater",
+    version="1.0",
+    author="Gianluca Lofrumento",
+    author_email="gianluca.lofrumento@gmail.com",
+    description="Update all the packages in pubspec.yaml of a Flutter project.",
+    scripts=["pubspec_update"],
+    requires=[
+        "aiohttp",
+        "argparse",
+        "asyncio",
+        "pyyaml",
+        "beautifulsoup4",
+        "tabulate"
+    ]
+)
